8133a265fbc33d506ac69697dcd753b8eec74c95
ptt08 <supersecureemailaccount@protonmail.com>
2023-07-16 20:49:02 +0900

    Variable and Data types

--- 01-Fundamentals-Part-1/starter/script.js
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -1,3 +1,14 @@
-let js = 'amazing';
-if (js === 'amazing') alert('Hello World!');
+let js = "amazing";
+// if (js === 'amazing') alert('Hello World!');
 console.log(69 + 420);
+
+let message = "Hello World!";
+console.log(message);
+
+// Good
+let myFirstJob = "Programmer"
+let myCurrentJob = "Teacher"
+
+// Bad
+let job1 = "programmer"
+let jo2 = "teacher"
---
+++ assignments.js
@@ -0,0 +1,16 @@
+// Values and variables
+let country = "Japan";
+let continent = "Asia";
+let population = 125_700_000;
+
+// console.log(country);
+// console.log(continent);
+// console.log(population);
+
+// Data types
+let isIsland = true;
+let language;
+console.log(isIsland);
+console.log(population);
+console.log(country);
+console.log(language);
