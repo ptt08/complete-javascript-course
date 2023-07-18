302a70a52532b0937bfffb1b6b50160c6c4a7f37
ptt08 <supersecureemailaccount@protonmail.com>
2023-07-18 19:55:10 +0900

    Arrays

--- 02-Fundamentals-Part-2/starter/script.js
+++ 02-Fundamentals-Part-2/starter/script.js
@@ -73,8 +73,6 @@ function fruitProcessor(apples, oranges) {
 
 console.log(fruitProcessor(2, 3));
 
-*/
-
 const calcAge = function (birthYear) {
     return 2037 - birthYear;
 }
@@ -98,3 +96,43 @@ const yearsUntilRetirement = function (birthYear, firstName) {
 
 console.log(yearsUntilRetirement(1992, 'John'));
 console.log(yearsUntilRetirement(1950, 'Mike'));
+
+const friend1 = 'Michael';
+const friend2 = 'Steven';
+const friend3 = 'Peter';
+
+const friends = ['Michael', 'Steven', 'Peter'];
+
+// const years = new Array(1991, 1984, 2002, 2020);
+
+console.log(friends[0]);
+console.log(friends[2]);
+
+console.log(friends.length);
+console.log(friends[friends.length - 1]);
+
+friends[2] = 'Jay';
+console.log(friends);
+
+const firstName = 'John';
+const john = [firstName, 'Doe', 2037 - 1991, 'teacher', friends];
+console.log(john);
+console.log(john.length)
+
+// Exercise
+
+const calcAge = function (birthYear) {
+    return 2037 - birthYear;
+}
+
+const years = [1990, 1967, 2002, 2010, 2018];
+
+const age1 = calcAge(years[0]);
+const age2 = calcAge(years[1]);
+const age3 = calcAge(years[years.length - 1]);
+console.log(age1, age2, age3);
+
+const ages = [age1, age2, age3];
+console.log(ages);
+
+*/
