c93a05b77504b629c77ca86a12f3c0f21c327682
ptt08 <supersecureemailaccount@protonmail.com>
2023-07-18 18:25:57 +0900

    Arrow functions

--- 02-Fundamentals-Part-2/starter/script.js
+++ 02-Fundamentals-Part-2/starter/script.js
@@ -27,8 +27,6 @@ const appleJuice = fruitProcessor(5, 0);
 
 console.log(appleJuice);
 console.log(fruitProcessor(5, 0));
-*/
-
 
 // Function declaration
 function calcAge1(birthYear) {
@@ -45,3 +43,20 @@ const calcAge2 = function (birthYear) {
 const age2 = calcAge2(1991);
 
 console.log(age1, age2);
+
+*/
+
+// Arrow function
+const calcAge3 = birthYear => 2038 - birthYear;
+const age3 = calcAge3(1991);
+console.log(age3);
+
+const yearsUntilRetirement = (birthYear, firstName) => {
+    const age = 2038 - birthYear;
+    const retirement = 65 - age;
+    // return retirement;
+    return `${firstName} retires in ${retirement} years`;
+}
+
+console.log(yearsUntilRetirement(1991, 'John'));
+console.log(yearsUntilRetirement(2000, 'Bob'));
