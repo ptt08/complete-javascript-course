854da32d72e0b82c5dc8585be01c2348e7d22222
ptt08 <supersecureemailaccount@protonmail.com>
2023-07-18 18:13:15 +0900

    Functions

--- 02-Fundamentals-Part-2/starter/script.js
+++ 02-Fundamentals-Part-2/starter/script.js
@@ -1,5 +1,5 @@
 'use strict';
-
+/*
 let hasDriversLicense = false;
 const passTest = true;
 
@@ -8,3 +8,23 @@ if (hasDriversLicense) console.log('I can drive :D');
 
 // const interface = 'Audio';
 // const private = 4343;
+*/
+
+function logger() {
+    console.log('My name is John');
+}
+
+// calling / running / invoking function
+logger();
+logger();
+logger();
+
+function fruitProcessor(apples, oranges) {
+    const juice = `Juice with ${apples} apples and ${oranges} oranges`;
+    return juice;
+}
+
+const appleJuice = fruitProcessor(5, 0);
+
+console.log(appleJuice);
+console.log(fruitProcessor(5, 0));
