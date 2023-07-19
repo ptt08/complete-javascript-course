2dab39c3f3d616258f53cf0ba78d486503ed25e7
ptt08 <supersecureemailaccount@protonmail.com>
2023-07-19 19:35:08 +0900

    Object methods

--- 02-Fundamentals-Part-2/starter/script.js
+++ 02-Fundamentals-Part-2/starter/script.js
@@ -165,7 +165,6 @@ console.log(friends.includes(23));
 if (friends.includes('Steven')) {
     console.log('You have a friend called Steven');
 }
-*/
 
 const john = {
     firstName: 'John',
@@ -201,3 +200,42 @@ console.log(john);
 const message = `${john.firstName} has ${john.friends.length} friends, and his best friend is called ${john.friends[0]}`;
 
 console.log(message);
+*/
+
+const john = {
+    firstName: 'John',
+    lastName: 'Doe',
+    birthYear: 2003,
+    job: 'teacher',
+    friends: ['Michael', 'Steven', 'Peter'],
+    hasDriversLicense: true,
+
+    // calcAge: function(birthYear) {
+    //     return 2037 - birthYear;
+    // }
+
+    // calcAge: function() {
+    //     // console.log(this);
+    //     return 2037 - this.birthYear;
+    // }
+
+    calcAge: function() {
+        this.age = 2037 - this.birthYear;
+        return this.age;
+    },
+
+    getSummary: function() {
+        return `${this.firstName} is a ${this.calcAge()}-year old ${this.job}, and he has ${this.hasDriversLicense ? 'a' : 'no'} driver's license`;
+    }
+};
+
+console.log(john.calcAge());
+
+console.log(john.age);
+console.log(john.age);
+console.log(john.age);
+
+// Challenge
+// "Jonas is a 46-year old teacher, and he has a driver's license"
+
+console.log(john.getSummary());
