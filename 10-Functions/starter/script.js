d56b05b611671e81d8bc1803a20986f39b54572b
ptt08 <76697922+ptt08@users.noreply.github.com>
2023-09-07 19:42:54 +0900

    functions returning functions

--- 10-Functions/starter/script.js
+++ 10-Functions/starter/script.js
@@ -1,5 +1,6 @@
 'use strict';
 
+/*
 const flight = 'LH234';
 const jonas = {
   name: 'Jonas Schmedtmann',
@@ -16,22 +17,22 @@ const checkIn = function (flightNum, passenger) {
     alert('Wrong passport!');
   }
 };
+*/
 
 // checkIn(flight, jonas);
-// console.log(flight);
 // console.log(jonas);
 
 // // Is the same as doing...
 // const flightNum = flight;
 // const passenger = jonas;
 
-const newPassport = function (person) {
-  person.passport = Math.trunc(Math.random() * 10000000000);
-};
+// const newPassport = function (person) {
+//   person.passport = Math.trunc(Math.random() * 10000000000);
+// };
 
-newPassport(jonas);
-checkIn(flight, jonas);
-console.log(jonas.passport);
+// newPassport(jonas);
+// checkIn(flight, jonas);
+// console.log(jonas.passport);
 /*
 const bookings = [];
 
@@ -60,3 +61,50 @@ createBooking('LH123', 5);
 
 createBooking('LH123', undefined, 1000);
 */
+
+/*
+const oneWord = function (str) {
+  return str.replace(/ /g, '').toLowerCase();
+};
+
+const upperFirstWord = function (str) {
+  const [first, ...others] = str.split(' ');
+  return [first.toUpperCase(), ...others].join(' ');
+};
+
+// Higher-order function
+const transformer = function (str, fn) {
+  console.log(`Original string: ${str}`);
+  console.log(`Transformed string: ${fn(str)}`);
+
+  console.log(`Transformed by: ${fn.name}`);
+};
+
+transformer('JavaScript is the best', upperFirstWord);
+console.log(''.padEnd(30, '-'));
+transformer('JavaScript is the best', oneWord);
+
+// JS uses callbacks all the time
+const high5 = function () {
+  console.log('👋');
+};
+
+document.body.addEventListener('click', high5);
+[1, 2, 3].forEach(high5);
+*/
+
+const greet = function (greeting) {
+  return function (name) {
+    console.log(`${greeting} ${name}`);
+  };
+};
+
+const greeterHey = greet('Hey');
+greeterHey('Jonas');
+greeterHey('Steven');
+
+greet('Hello')('Jonas');
+
+const greetArrow = greeting => name => console.log(`${greeting} ${name}`);
+
+greetArrow('Good morning')('John');
