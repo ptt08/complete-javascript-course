e54be54f4b745b3a2402502c4f61b87e7eeb2ca2
ptt08 <76697922+ptt08@users.noreply.github.com>
2023-09-13 15:56:51 +0900

    map

--- 11-Arrays-Bankist/starter/script.js
+++ 11-Arrays-Bankist/starter/script.js
@@ -82,6 +82,19 @@ const displayMovements = function (movements) {
 };
 displayMovements(account1.movements);
 
+const createUsernames = function (accs) {
+  accs.forEach(acc => {
+    const username = acc.owner
+      .toLowerCase()
+      .split(' ')
+      .map(e => e[0])
+      .join('');
+    acc.username = username;
+  });
+};
+
+createUsernames(accounts);
+
 /////////////////////////////////////////////////
 /////////////////////////////////////////////////
 // LECTURES
@@ -92,7 +105,7 @@ displayMovements(account1.movements);
 //   ['GBP', 'Pound sterling'],
 // ]);
 
-// const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
+const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
 
 /////////////////////////////////////////////////
 /*
@@ -206,7 +219,7 @@ TEST DATA 2: Julia's data [9, 16, 6, 8, 3], Kate's data [10, 5, 6, 1, 4]
 
 GOOD LUCK 😀
 */
-
+/*
 const checkDogs = function (dogsJulia, dogsKate) {
   const newDogsJulia = dogsJulia.slice(1, -2);
   const dogAges = [...newDogsJulia, ...dogsKate];
@@ -223,3 +236,29 @@ console.log('------------TEST 1-------------');
 checkDogs([3, 5, 2, 12, 7], [4, 1, 15, 8, 3]);
 console.log('------------TEST 2-------------');
 checkDogs([9, 16, 6, 8, 3], [10, 5, 6, 1, 4]);
+*/
+/*
+const eurToUsd = 1.1;
+// const movementsUSD = movements.map(function(mov) {
+//   return mov * eurToUsd;
+// });
+
+const movementsUSD = movements.map(mov => mov * eurToUsd);
+
+console.log(movements);
+console.log(movementsUSD);
+
+const movementsUSDfor = [];
+
+for (const mov of movements) movementsUSDfor.push(mov * eurToUsd);
+
+console.log(movementsUSDfor);
+
+const movementsDescriptions = movements.map((mov, i) => {
+  return `Movement ${i + 1}: You ${
+    mov > 0 ? 'deposited' : 'withdrew'
+  } ${Math.abs(mov)}`;
+});
+
+console.log(movementsDescriptions);
+*/
