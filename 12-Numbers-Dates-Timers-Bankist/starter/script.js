4180c970f2de0d5f0add3462f91dab5e29ad830f
ptt08 <supersecureemailaccount@protonmail.com>
2023-09-21 16:31:50 +0900

    Dates

--- 12-Numbers-Dates-Timers-Bankist/starter/script.js
+++ 12-Numbers-Dates-Timers-Bankist/starter/script.js
@@ -358,6 +358,7 @@ labelBalance.addEventListener('click', function () {
 // const diameter = 287_460_000_000;
 // console.log(diameter);
 
+/*
 console.log(2 ** 53 - 1);
 console.log(Number.MAX_SAFE_INTEGER);
 console.log(2 ** 53 + 1);
@@ -391,3 +392,38 @@ console.log(huge + ' is REALLY big');
 // Divisions
 console.log(11n / 3n);
 console.log(10 / 3);
+*/
+
+// Create a date
+// const now = new Date();
+// console.log(now);
+
+// console.log(new Date('Sep 21 2023 16:18:45'));
+// console.log(new Date('December 4, 2005'));
+// console.log(new Date(account1.movementsDates[0]));
+
+// console.log(new Date(2037, 10, 32));
+
+// console.log(new Date(0));
+// console.log(new Date(3 * 24 * 60 * 60 * 1000));
+
+// Working with dates
+
+const future = new Date(2037, 10, 19, 15, 23);
+console.log(future);
+console.log(future.getFullYear());
+console.log(future.getMonth());
+console.log(future.getDate());
+console.log(future.getDay());
+console.log(future.getHours());
+console.log(future.getMinutes());
+console.log(future.getSeconds());
+console.log(future.toISOString());
+console.log(future.getTime());
+
+console.log(new Date(2142224580000));
+
+console.log(Date.now());
+
+future.setFullYear(2050);
+console.log(future);
