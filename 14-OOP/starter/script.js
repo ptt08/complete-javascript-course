04fa2368344f5f95fbd14693916d546de7e41d49
ptt08 <supersecureemailaccount@protonmail.com>
2023-10-14 15:40:32 +0900

    oop

--- 14-OOP/starter/script.js
+++ 14-OOP/starter/script.js
@@ -1,1 +1,26 @@
 'use strict';
+
+const Person = function (firstName, birthYear) {
+  // Instance properties
+  this.firstName = firstName;
+  this.birthYear = birthYear;
+
+  // Never do this
+  // this.calcAge = function () {
+  //   console.log(2037 - this.birthYear);
+  // };
+};
+
+const lucca = new Person('Lucca', 2002);
+console.log(lucca);
+
+// 1. New {} is created
+// 2. function is called, this = {}
+// 3. {} linked to prototype
+// 4. function automatically returns {}
+
+const john = new Person('John', 1992);
+const matilda = new Person('Matilda', 2000);
+console.log(john, matilda);
+
+console.log(john instanceof Person);
