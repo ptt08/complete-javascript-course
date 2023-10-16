3e591027636d7715f3fcc37842c649a5b7d7232e
ptt08 <supersecureemailaccount@protonmail.com>
2023-10-16 18:46:51 +0900

    Object.create

--- 14-OOP/starter/script.js
+++ 14-OOP/starter/script.js
@@ -172,6 +172,7 @@ jessica.greet();
 
 const walter = new PersonCl('Walter White', 1965);
 
+/*
 const account = {
   owner: 'jonas',
   movements: [100, 232, 345, 1001],
@@ -189,3 +190,28 @@ console.log(account.latest);
 
 account.latest = 50;
 console.log(account.movements);
+*/
+
+const PersonProto = {
+  calcAge() {
+    console.log(2037 - this.birthYear);
+  },
+
+  init(firstName, birthYear) {
+    this.firstName = firstName;
+    this.birthYear = birthYear;
+  },
+};
+
+const steven = Object.create(PersonProto);
+console.log(steven);
+steven.name = 'Steven';
+steven.birthYear = 2002;
+
+steven.calcAge();
+
+console.log(steven.__proto__ === PersonProto);
+
+const sarah = Object.create(PersonProto);
+sarah.init('Sarah', 1997);
+sarah.calcAge();
