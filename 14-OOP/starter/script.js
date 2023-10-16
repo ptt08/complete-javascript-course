01c31f177bd58c5f900abe5e3e232cd53055640e
ptt08 <supersecureemailaccount@protonmail.com>
2023-10-16 19:44:36 +0900

    inheritance - constructor functions

--- 14-OOP/starter/script.js
+++ 14-OOP/starter/script.js
@@ -1,77 +1,77 @@
 'use strict';
 
-const Person = function (firstName, birthYear) {
-  // Instance properties
-  this.firstName = firstName;
-  this.birthYear = birthYear;
+// const Person = function (firstName, birthYear) {
+//   // Instance properties
+//   this.firstName = firstName;
+//   this.birthYear = birthYear;
 
-  // Never do this
-  // this.calcAge = function () {
-  //   console.log(2037 - this.birthYear);
-  // };
-};
+//   // Never do this
+//   // this.calcAge = function () {
+//   //   console.log(2037 - this.birthYear);
+//   // };
+// };
 
-const lucca = new Person('Lucca', 2002);
-console.log(lucca);
+// const lucca = new Person('Lucca', 2002);
+// console.log(lucca);
 
-// 1. New {} is created
-// 2. function is called, this = {}
-// 3. {} linked to prototype
-// 4. function automatically returns {}
+// // 1. New {} is created
+// // 2. function is called, this = {}
+// // 3. {} linked to prototype
+// // 4. function automatically returns {}
 
-const john = new Person('John', 1992);
-const matilda = new Person('Matilda', 2000);
-console.log(john, matilda);
+// const john = new Person('John', 1992);
+// const matilda = new Person('Matilda', 2000);
+// console.log(john, matilda);
 
-console.log(john instanceof Person);
+// console.log(john instanceof Person);
 
-Person.hey = function () {
-  console.log('Hey there 👋');
-  console.log(this);
-};
-Person.hey();
+// Person.hey = function () {
+//   console.log('Hey there 👋');
+//   console.log(this);
+// };
+// Person.hey();
 
 ///////////////////////////////////////////
 // Prototypes
-console.log(Person.prototype);
+// console.log(Person.prototype);
 
-Person.prototype.calcAge = function () {
-  console.log(2037 - this.birthYear);
-};
+// Person.prototype.calcAge = function () {
+//   console.log(2037 - this.birthYear);
+// };
 
-lucca.calcAge();
+// lucca.calcAge();
 
-console.log(lucca.__proto__ === Person.prototype);
-console.log(Person.prototype.isPrototypeOf(lucca));
+// console.log(lucca.__proto__ === Person.prototype);
+// console.log(Person.prototype.isPrototypeOf(lucca));
 
-Person.prototype.species = 'Homo Sapiens';
-console.log(lucca.species, matilda.species);
+// Person.prototype.species = 'Homo Sapiens';
+// console.log(lucca.species, matilda.species);
 
-console.log(lucca.hasOwnProperty('firstName'));
-console.log(lucca.hasOwnProperty('species'));
+// console.log(lucca.hasOwnProperty('firstName'));
+// console.log(lucca.hasOwnProperty('species'));
 
-console.log(lucca.__proto__);
-// Object.prototype (top of prototype chain)
-console.log(lucca.__proto__.__proto__);
-console.log(lucca.__proto__.__proto__.__proto__);
+// console.log(lucca.__proto__);
+// // Object.prototype (top of prototype chain)
+// console.log(lucca.__proto__.__proto__);
+// console.log(lucca.__proto__.__proto__.__proto__);
 
-console.log(Person.prototype.constructor.__proto__);
+// console.log(Person.prototype.constructor.__proto__);
 
-const arr = [1, 23, 3, 4, 5, 6, 7, 4, 3, 2, 1]; // new Array === []
+// const arr = [1, 23, 3, 4, 5, 6, 7, 4, 3, 2, 1]; // new Array === []
 
-console.log(arr.__proto__);
-console.log(arr.__proto__ === Array.prototype);
+// console.log(arr.__proto__);
+// console.log(arr.__proto__ === Array.prototype);
 
-console.log(arr.__proto__.__proto__);
+// console.log(arr.__proto__.__proto__);
 
-Array.prototype.unique = function () {
-  return [...new Set(this)];
-};
+// Array.prototype.unique = function () {
+//   return [...new Set(this)];
+// };
 
-console.log(arr.unique());
+// console.log(arr.unique());
 
-const h1 = document.querySelector('h1');
-console.dir(x => x + 1);
+// const h1 = document.querySelector('h1');
+// console.dir(x => x + 1);
 
 ///////////////////////////////////////
 // Coding Challenge #1
@@ -262,3 +262,38 @@ GOOD LUCK 😀
 
 // ford.speedUS = 50;
 // console.log(ford);
+
+const Person = function (firstName, birthYear) {
+  this.firstName = firstName;
+  this.birthYear = birthYear;
+};
+
+Person.prototype.calcAge = function () {
+  console.log(2037 - this.birthYear);
+};
+
+const Student = function (firstName, birthYear, course) {
+  Person.call(this, firstName, birthYear);
+  this.course = course;
+};
+
+// Linking prototypes
+Student.prototype = Object.create(Person.prototype);
+// Student.prototype = Person.prototype; BAD
+
+Student.prototype.introduce = function () {
+  console.log(`My name is ${this.firstName} and I study ${this.course}`);
+};
+
+const mike = new Student('Mike', 2020, 'Computer Science');
+mike.introduce();
+mike.calcAge();
+
+console.log(mike.__proto__);
+console.log(mike.__proto__.__proto__);
+
+console.log(mike instanceof Student);
+console.log(mike instanceof Person);
+
+Student.prototype.constructor = Student;
+console.dir(Student.prototype.constructor);
