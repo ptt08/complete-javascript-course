a0d539c5c89684acbc02ea6e1186aa8570a28d3a
ptt08 <supersecureemailaccount@protonmail.com>
2023-11-02 22:34:29 +0900

    The module pattern

--- 17-Modern-JS-Modules-Tooling/starter/script.js
+++ 17-Modern-JS-Modules-Tooling/starter/script.js
@@ -3,7 +3,7 @@
 // addToCart('banana', 20);
 // console.log(price, tq);
 
-console.log('Importing module');
+// console.log('Importing module');
 
 // import * as ShoppingCart from './shoppingCart.js';
 // ShoppingCart.addToCart('bread', 5);
@@ -11,6 +11,7 @@ console.log('Importing module');
 
 // import add, { addToCart, totalPrice as price, tq } from './shoppingCart.js';
 
+/*
 import add, { cart } from './shoppingCart.js';
 add('pizza', 2);
 add('bread', 5);
@@ -40,3 +41,36 @@ console.log(lastPost);
 
 const lastPost2 = await getLastPost();
 console.log(lastPost2);
+*/
+
+const ShoppingCart2 = (function () {
+  const cart = [];
+  const shippingCost = 10;
+  const totalPrice = 237;
+  const totalQuantity = 23;
+
+  const addToCart = function (product, quantity) {
+    cart.push({ product, quantity });
+    console.log(
+      `${quantity} ${product} added to cart (shipping cost is ${shippingCost})`
+    );
+  };
+
+  const orderStock = function (product, quantity) {
+    cart.push({ product, quantity });
+    console.log(`${quantity} ${product} ordered from supplier`);
+  };
+
+  return {
+    addToCart,
+    cart,
+    totalPrice,
+    totalQuantity,
+  };
+})();
+
+ShoppingCart2.addToCart('apple', 4);
+ShoppingCart2.addToCart('pizza', 2);
+
+console.log(ShoppingCart2);
+console.log(ShoppingCart2.shippingCost);
