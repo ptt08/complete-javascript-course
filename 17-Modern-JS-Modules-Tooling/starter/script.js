75675d48486238603753a5139ec8dc2af2583a7b
ptt08 <supersecureemailaccount@protonmail.com>
2023-11-03 17:42:48 +0900

    Intro to NPM

--- 17-Modern-JS-Modules-Tooling/starter/script.js
+++ 17-Modern-JS-Modules-Tooling/starter/script.js
@@ -87,3 +87,20 @@ export.addToCart = function (product, quantity) {
 // Import
 const { addToCart } = require("./shoppingCart.js");
 */
+
+import cloneDeep from './node_modules/lodash-es/cloneDeep.js';
+
+const state = {
+  cart: [
+    { product: 'bread', quantity: 5 },
+    { product: 'pizza', quantity: 5 },
+  ],
+  user: { loggedIn: true },
+};
+
+const stateDeepClone = cloneDeep(state);
+const stateClone = Object.assign({}, state);
+state.user.loggedIn = false;
+
+console.log(stateClone);
+console.log(stateDeepClone);
